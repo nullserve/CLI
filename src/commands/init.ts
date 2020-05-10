657069ae9e2cf9acce64edc1ac8ab86459e0d1dc
David J. Felix <felix.davidj@gmail.com>
2020-05-09 23:15:22 -0400

    feat: create deploy command

--- src/commands/init.ts
+++ src/commands/init.ts
@@ -1,9 +1,12 @@
 import {Command, flags} from '@oclif/command'
-import {existsSync, mkdirSync, writeFileSync} from 'fs'
+import {existsSync, mkdirSync, writeFileSync, readFileSync, exists} from 'fs'
 import {prompt} from 'inquirer'
+import axios, {AxiosResponse} from 'axios'
+
+import {Config} from '../config'
 
 export default class Init extends Command {
-  static description = 'init a nullserve project'
+  static description = 'initialize a nullserve project'
 
   static examples = [`$ nullserve init`]
 
@@ -37,6 +40,13 @@ export default class Init extends Command {
 
   async run() {
     const {args, flags} = this.parse(Init)
+    let userConfig: Config
+
+    if (existsSync(this.config.configDir)) {
+      userConfig = JSON.parse(readFileSync(this.config.configDir, 'utf-8'))
+    } else {
+      userConfig = {}
+    }
 
     let promptResponses = await prompt([
       {
@@ -51,13 +61,24 @@ export default class Init extends Command {
         ],
       },
     ])
-
     if (promptResponses.shouldCreateProject === 'cancel') {
       console.log('Cancelled init.')
       return
     }
-
     const shouldCreateProject = promptResponses.shouldCreateProject === 'yes'
+    let apiToken: string
+
+    if (flags.token !== undefined && flags.token !== null) {
+      apiToken = flags.token
+    } else if (
+      userConfig.apiToken !== undefined &&
+      userConfig.apiToken !== null
+    ) {
+      apiToken = userConfig.apiToken
+    } else {
+      console.log('Need to create an api token')
+      return
+    }
 
     if (!existsSync('./.nullserve')) {
       mkdirSync('./.nullserve')
@@ -68,6 +89,18 @@ export default class Init extends Command {
         './.nullserve/routes.json',
         JSON.stringify(this.defaultRoutes),
       )
+    }
+
+    if (shouldCreateProject) {
+      const createSite = await axios.post(
+        `https://api.nullserve.com/graph`,
+        {
+          query: `mutation {createSite(urlSlug: "${flags['site-slug']}") {urlSlug}}`,
+          variables: {},
+        },
+        {headers: {Authorization: `Bearer ${apiToken}`}},
+      )
+      console.log(createSite.data)
     }
   }
 }
